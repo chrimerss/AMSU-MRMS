a3b1c1aa4a4ac3ad30f6bfe67a5417c54a2c45a1
Hydros OU <hydrosou@d-ip-10-197-1-70.nwc.nor.ou.edu>
2019-12-14 20:41:09 -0600

    fix model bug

--- model.py
+++ model.py
@@ -18,15 +18,12 @@ def benchmark():
             first=False
         else:
             param.requires_grad= False
-    model.add_module(
-        nn.Sequential(
-            nn.Conv2d(512,64,3,1,1),
-            nn.ReLU(True),
-            nn.Conv2d(64, 16, 3,1,1),
-            nn.ReLU(True),
-            nn.Conv2d(16,1,3,1,1),
-            nn.ReLU(True)
-        )
-    )
+    model.add_module('active_conv1', nn.Conv2d(512,64,3,1,1))
+    model.add_module('active_relu1', nn.ReLU(True))
+    model.add_module('active_conv2', nn.Conv2d(64, 16, 3,1,1))
+    model.add_module('active_relu1', nn.ReLU(True))
+    model.add_module('active_conv1', nn.Conv2d(16,1,3,1,1))
+    model.add_module('active_relu1', nn.ReLU(True))
+
 
     return model
